daac40b72516581d1cfe652c450084db5b67ed04
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2022-02-20 12:56:43 -0500

    Permite unirse a salas desde el cliente y tener estadisticas

--- http.js
+++ http.js
@@ -5,24 +5,55 @@ var body_parser = require('body-parser');
 var app = express();
 app.use(body_parser.urlencoded({extended: true}));
 var http = require('http').Server(app);
-var io = require('socket.io')(http,  { cors: { origin: '*' } });
+var io = require('socket.io')(http, {
+    cors: {
+        origin: '*'
+    }
+});
 
-app.get('/send', function (req, res) { 
-    let token = req.query.token;   
+app.get('/send', function (req, res) {
+    let token = req.query.token;
     let content = req.query.content;
     let channel = req.query.channel;
+    let event = req.query.event;
     let secret_token = 'asdasd';
 
     if (secret_token === token && content !== '') {
-          io.emit(channel, content);
-          res.send('Notificacion enviada');       
+        io.to(channel).emit(event, content);
+        res.send('Notificacion enviada');
     } else {
         res.send('No autorizado');
     }
 });
 
+var count_channels = {};
+var sockets = {};
+
+app.get('/clients', function (req, res) {
+    res.send('[{"total": ' + count_channels[req.query.channel] + '}]');
+});
+
+
 io.on("connection", function (socket) {
-   //
+
+    socket.on('join', (channel) => {
+        socket.join(channel);
+        sockets[socket.id] = channel;
+        socket.emit('log', 'Se ha unido a la sala: ' + channel);
+        if (typeof count_channels[channel] === "undefined") {
+            count_channels[channel] = 0;
+        }
+        count_channels[channel]++;
+    });
+
+    socket.on("disconnect", (reason) => {
+        if (typeof sockets[socket.id] !== "undefined") {
+            let channel = sockets[socket.id];
+            count_channels[channel]--;
+            delete sockets[socket.id];
+        }
+    });
+
 });
 
 
--- https.js
+++ https.js
@@ -5,33 +5,62 @@ var app = express();
 app.use(body_parser.urlencoded({extended: true}));
 
 const httpServer = require("https").createServer({
-  key: fs.readFileSync("/etc/letsencrypt/live/testing.medislove.com/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/testing.medislove.com/fullchain.pem")
+    key: fs.readFileSync("/etc/letsencrypt/live/testing.medislove.com/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/testing.medislove.com/fullchain.pem")
 }, app);
 
-app.get('/send', function (req, res) { 
-    let token = req.query.token;   
+app.get('/send', function (req, res) {
+    let token = req.query.token;
     let content = req.query.content;
     let channel = req.query.channel;
     let secret_token = 'asdasd';
 
     if (secret_token === token && content !== '') {
-          io.emit(channel, content);
-          res.send('Notificacion enviada');       
+        io.emit(channel, content);
+        res.send('Notificacion enviada');
     } else {
         res.send('No autorizado');
     }
 });
 
-const options = { 
-        cors: {
-                origin: "*",
-        },
+const options = {
+    cors: {
+        origin: "*"
+    }
 };
 
 const io = require("socket.io")(httpServer, options);
 
-io.on("connection", socket => { /* ... */ });
+var count_channels = {};
+var sockets = {};
 
-httpServer.listen(3001);
+app.get('/clients', function (req, res) {
+    res.send('[{"total": ' + count_channels[req.query.channel] + '}]');
+});
 
+
+io.on("connection", function (socket) {
+
+    socket.on('join', (channel) => {
+        socket.join(channel);
+        sockets[socket.id] = channel;
+        socket.emit('log', 'Se ha unido a la sala: ' + channel);
+        if (typeof count_channels[channel] === "undefined") {
+            count_channels[channel] = 0;
+        }
+        count_channels[channel]++;
+    });
+
+    socket.on("disconnect", (reason) => {
+        if (typeof sockets[socket.id] !== "undefined") {
+            let channel = sockets[socket.id];
+            count_channels[channel]--;
+            delete sockets[socket.id];
+        }
+    });
+
+});
+
+httpServer.listen(3001, function () {
+    console.log("Servidor corriendo en http://localhost:3001");
+});
